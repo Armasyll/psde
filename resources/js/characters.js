e7f7744bb2ab7b105a9961a2fa2efbb306c9d2ef
armasyll <armasyll@gmail.com>
2020-06-06 16:54:51 -0400

    Just a random character named harley

--- resources/js/characters.js
+++ resources/js/characters.js
@@ -1,6 +1,7 @@
 Game.createCharacterEntity("rinehart", "Rinehart Nye", undefined, "genericCharacterIcon", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FOX, SexEnum.MALE, 30, "foxM", "foxRinehart", {eyes:EyeEnum.CIRCLE, eyesColour:"violet"});
 Game.createCharacterEntity("rosie", "Rosie", undefined, "rosieIcon", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FOX, SexEnum.FEMALE, 14, "foxF", "foxRed", {eyes:EyeEnum.CIRCLE, eyesColour:"blue"});
 Game.createCharacterEntity("charlie", "Charlie", undefined, "charlieIcon", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FOX, SexEnum.FEMALE, 28, "foxF", "foxCorsac", {eyes:EyeEnum.FERAL, eyesColour:"blue"});
+Game.createCharacterEntity("harley", "Harley", undefined, "harleyIcon", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FENNECFOX, SexEnum.FEMALE, 23, "foxHarley", {eyes:EyeEnum.CIRCLE, eyesColour:"brown"})
 
 /*CharacterController.get("charlie").setIdleAnim("90_idleSquint01", 1, true);
 CharacterController.get("charlie").setRunAnim("94_runningKneesBentSquint", 1, true);*/
