9cd5c40a9947fd2c2421c821a2235b277015a17a
armasyll <armasyll@gmail.com>
2021-05-25 14:41:50 -0400

    Moved over to smaller textures, and fixed typo

--- resources/js/characters.js
+++ resources/js/characters.js
@@ -1,7 +1,7 @@
 let rinehart = EntityLogic.createCharacterEntity("rinehart", "Rinehart Nye", undefined, "genericCharacterIcon", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FOX, SexEnum.MALE, 30, "foxM", "foxRinehart", { eyes: EyeEnum.CIRCLE, eyesColour: "violet" });
-let rosie = EntityLogic.createCharacterEntity("rosie", "Rosie", undefined, "rosieIcon", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FOX, SexEnum.FEMALE, 14, "foxF", "foxRed", { eyes: EyeEnum.CIRCLE, eyesColour: "blue" });
+let rosie = EntityLogic.createCharacterEntity("rosie", "Rosie", undefined, "genericCharacter", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FOX, SexEnum.FEMALE, 14, "foxF", "foxRed", { eyes: EyeEnum.CIRCLE, eyesColour: "blue" });
 let charlie = EntityLogic.createCharacterEntity("charlie", "Charlie", undefined, "charlieIcon", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FOX, SexEnum.FEMALE, 28, "foxF", "foxCorsac", { eyes: EyeEnum.SLIT, eyesColour: "blue" });
-let harley = EntityLogic.createCharacterEntity("harley", "Harley", undefined, "harleyIcon", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FENNECFOX, SexEnum.FEMALE, 23, "foxHarley", { eyes: EyeEnum.CIRCLE, eyesColour: "brown" })
+let harley = EntityLogic.createCharacterEntity("harley", "Harley", undefined, "genericRabbit", CreatureTypeEnum.HUMANOID, CreatureSubTypeEnum.FENNECFOX, SexEnum.FEMALE, 23, "foxHarley", { eyes: EyeEnum.CIRCLE, eyesColour: "brown" })
 
 /*CharacterController.get("charlie").setIdleAnim("90_idleSquint01", 1, true);
 CharacterController.get("charlie").setRunAnim("94_runningKneesBentSquint", 1, true);*/
