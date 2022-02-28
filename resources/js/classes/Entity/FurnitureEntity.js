46c5306409310d78b852fcce2274e778f0205974
armasyll <armasyll@gmail.com>
2022-02-27 20:40:16 -0500

    Open furniture, just in case :v

--- resources/js/classes/Entity/FurnitureEntity.js
+++ resources/js/classes/Entity/FurnitureEntity.js
@@ -16,6 +16,7 @@ class FurnitureEntity extends Entity {
         this.entityType = EntityEnum.FURNITURE;
         this.furnitureType = FurnitureEnum.NONE;
         this.availableSeats = 0;
+        this.open = false;
 
         this.setFurnitureType(furnitureType);
 
@@ -179,6 +180,21 @@ class FurnitureEntity extends Entity {
     }
     getAvailableSeats() {
         return this.availableSeats;
+    }
+    setOpen() {
+        this.open = true;
+        this.removeHiddenAvailableAction(ActionEnum.CLOSE);
+        this.setDefaultAction(ActionEnum.CLOSE);
+        this.addHiddenAvailableAction(ActionEnum.OPEN);
+    }
+    setClose() {
+        this.open = false;
+        this.setDefaultAction(ActionEnum.OPEN);
+        this.removeHiddenAvailableAction(ActionEnum.OPEN);
+        this.addHiddenAvailableAction(ActionEnum.CLOSE);
+    }
+    getOpen() {
+        return this.open;
     }
 
     objectifyMinimal() {
