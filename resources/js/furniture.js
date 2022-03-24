18fe6665506d9e9c84d04dd8e6bfe0e4496d52fe
armasyll <armasyll@gmail.com>
2022-03-24 16:10:50 -0400

    Refrigerator UV and texture

--- resources/js/furniture.js
+++ resources/js/furniture.js
@@ -1,7 +1,7 @@
 EntityLogic.createFurnitureEntity("mattress01", "Mattress", "A plain mattress.", "mattress01Icon", "mattress01", undefined, FurnitureEnum.BED, 20, 65);
 EntityLogic.createFurnitureEntity("animatedToilet01", "Toilet", "An ordinary, simple toilet.", "toilet01Icon", "animatedToilet01", "ceramicsAndPipes", FurnitureEnum.TOILET, 55, 300);
 EntityLogic.createFurnitureEntity("animatedSink01Faucet", "Faucet", "An ordinary, simple faucet.", "sink01Icon", "animatedSink01Faucet", "ceramicsAndPipes", FurnitureEnum.SINK, 45, 200);
-EntityLogic.createFurnitureEntity("animatedRefrigerator01", "Refrigerator", "A refrigerator.", "refrigerator01Icon", "animatedRefrigerator01", undefined, FurnitureEnum.REFRIGERATOR, 170, 800);
+EntityLogic.createFurnitureEntity("animatedRefrigerator01", "Refrigerator", "A refrigerator.", "refrigerator01Icon", "animatedRefrigerator01", "beigeRefrigerator01", FurnitureEnum.REFRIGERATOR, 170, 800);
 EntityLogic.createFurnitureEntity("couch01", "Couch", "A couch.", "couch01Icon", "couch01", undefined, FurnitureEnum.COUCH, 120, 200);
 EntityLogic.createFurnitureEntity("couch02", "Couch", "A couch.", "couch02Icon", "couch02", undefined, FurnitureEnum.COUCH, 120, 250);
 EntityLogic.createFurnitureEntity("loveseat01", "Loveseat", "A loveseat.", "loveseat01Icon", "loveseat01", undefined, FurnitureEnum.LOVESEAT, 80, 150);
