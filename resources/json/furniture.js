8905d4441e32169a7e0e50c51a19d8760897c8a0
armasyll <armasyll@gmail.com>
2022-07-23 23:48:54 -0400

    Road :v not implementing yet 'cause idk wat do :V

--- resources/json/furniture.js
+++ resources/json/furniture.js
@@ -2,9 +2,9 @@ EntityLogic.createFurnitureEntity("mattress01", "Mattress", "A plain mattress.",
 EntityLogic.createFurnitureEntity("animatedToilet01", "Toilet", "An ordinary, simple toilet.", "toilet01Icon", "animatedToilet01", "ceramicsAndPipes", FurnitureEnum.TOILET, 55, 300);
 EntityLogic.createFurnitureEntity("animatedSink01Faucet", "Faucet", "An ordinary, simple faucet.", "sink01Icon", "animatedSink01Faucet", "ceramicsAndPipes", FurnitureEnum.SINK, 45, 200);
 EntityLogic.createFurnitureEntity("animatedRefrigerator01", "Refrigerator", "A refrigerator.", "refrigerator01Icon", "animatedRefrigerator01", "beigeRefrigerator01", FurnitureEnum.REFRIGERATOR, 170, 800);
-EntityLogic.createFurnitureEntity("couch01", "Couch", "A couch.", "couch01Icon", "couch01", undefined, FurnitureEnum.COUCH, 120, 200);
-EntityLogic.createFurnitureEntity("loveseat01", "Loveseat", "A loveseat.", "loveseat01Icon", "loveseat01", undefined, FurnitureEnum.LOVESEAT, 80, 150);
-EntityLogic.createFurnitureEntity("chair01", "Chair", "A comfortable chair.", "chair01Icon", "chair01", undefined, FurnitureEnum.CHAIR, 40, 125);
+EntityLogic.createFurnitureEntity("couch01", "Couch", "A couch.", "couch01Icon", "couch01", "upholsteryGreen", FurnitureEnum.COUCH, 120, 200);
+EntityLogic.createFurnitureEntity("loveseat01", "Loveseat", "A loveseat.", "loveseat01Icon", "loveseat01", "upholsteryGreen", FurnitureEnum.LOVESEAT, 80, 150);
+EntityLogic.createFurnitureEntity("chair01", "Chair", "A comfortable chair.", "chair01Icon", "chair01", "upholsteryGreen", FurnitureEnum.CHAIR, 40, 125);
 EntityLogic.createFurnitureEntity("chair02", "Chair", "A simple chair.", "chair02Icon", "chair02", undefined, FurnitureEnum.CHAIR, 10, 15);
 EntityLogic.createFurnitureEntity("chair03", "Chair", "A chair.", "chair03Icon", "chair03", undefined, FurnitureEnum.CHAIR, 10, 10);
 EntityLogic.createFurnitureEntity("flatScreenMonitor01", "TV", "A television.", undefined, ["flatScreenMonitor01", "flatScreenMonitor01Screen"], undefined, FurnitureEnum.TELEVISION, 30, 180);
